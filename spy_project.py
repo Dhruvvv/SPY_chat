692f4139886fa612dadf44cea99e1d6366c00b88
Dhruvvv <tomardhruv019@gmail.com>
2017-06-28 21:02:28 +0530

    new function

--- spy_project.py
+++ spy_project.py
@@ -1,11 +1,11 @@
 spy_detail={}
 spy_status_history={}
-status=["Available","sleeping"],"Busy","online"]
+status=["Available","sleeping","Busy","online"]
 special_words=["sos","save_me","help_me"]
 count=0
 
 def spy_friend(user_name):
-    f_name=raw_input("Enter the friend name: ")
+    f_name=raw_input("Enter the friend name:")
     if len(f_name)!=0:
         f_age = int(raw_input("Enter your friend age"))
         if f_age > 12 and f_age < 50:
@@ -52,15 +52,10 @@ def select_a_friend(user_name):
 
 choice=True
 while choice==True:
-    user_choice=int(raw_input("Do you want to continue using \n1.Default user \n2.Create a new spy_useruser"))
+    user_choice=int(raw_input("Do you want to continue using \n1.Default user \n2.Create a new spy_user"))
     if user_choice==1:
         import spy_details
         user_name=spy_details.default_spy_details.keys()[0]
-        if user_name in spy_details.keys():
-            print "Default user exist in spy_dictionary"
-        else:
-            spy_details.update(spy_details.default_spy_detail)
-            spy_status_history.update({user_name: []})
     elif user_choice==2:
         user_name=raw_input("Enter the user")
         if len(user_name)<1:
@@ -71,12 +66,10 @@ while choice==True:
             age=int(raw_input("enter the age of spy"))
             if 12>=age<=50:
                 ratings = float(raw_input("enter the ratings"))
-                if ratings == 1:
-                    print "Not good!"
-                elif ratings > 1 and ratings <= 5:
-                    print  "Good"
-                elif ratings > 5 and ratings <= 10:
-                    print "Excellent"
+                if ratings<0:
+                    print "sir please enter ratings in positive"
+                elif ratings > 10:
+                    print "Rating cannot be greater"
                 else:
                     print "Wrong choice!"
                 spy_details.update({user_name:dict(salutation=input_salutation,age=age,status=status,ratings=ratings)})
@@ -95,18 +88,18 @@ while choice==True:
     if status_menu == 1:
         spy_status(user_name)
     elif status_menu == 2:
-        friennds_count=spy_friend(user_name)
+        friends_count=spy_friend(user_name)
         print  "Your %i friends as" %(friends_count)
     elif status_menu == 3:
         select_a_friend()
     elif status_menu == 4:
-        read_messsage(user_name)
+        read_message(user_name)
     elif status_menu == 5:
         chat_read(user_name)
     elif status_menu==6:
           exit()
     else:
         print "Invalid input"
-
-
+        continue
+exit()
 
